19876638bc2d410ea6b6b938f90cb90454fa10ee
Ramon Bezerra da Nóbrega <ramon.bezerra@outlook.com>
2020-11-20 21:30:00 -0300

    forms and validation fields with formik+yup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './lesson5/App';
+import App from './lesson6/App';
 
 ReactDOM.render(<App name="Ramon"/>, document.getElementById('root'));
